f5f23637c73b64e8b275ad32361216d31b124b49
Santhosh Kumar Devadoss <santhoshkumardeva25@gmail.com>
2025-10-02 14:31:46 +0530

    Update ubuntu-prerequisite-install.sh

--- ubuntu-prerequisite-install.sh
+++ ubuntu-prerequisite-install.sh
@@ -5,10 +5,21 @@ exec  2> >(tee -ia /var/log/pre-requisite-install.log >& 2)
 exec 19>> /var/log/pre-requisite-install.log
 
 
+echo "Installing pre-requisites"
+echo "========================"
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get update
 sudo apt-get update && sudo apt-get install -y bash jq zstd rsync systemd-timesyncd conntrack iptables rsyslog --no-install-recommends
-sudo mkdir -p /etc/netplan/backup
 
+echo "Disabling netplan"
+echo "========================"
+sudo mkdir -p /etc/netplan/backup
+sudo mv /etc/netplan/*.yaml /etc/netplan/backup/
+sudo apt-get purge -y netplan.io
+sudo touch /etc/butt/butt-init.disabled
+
+
+echo "Configuring systemd-networkd"
+echo "========================"
 cat > /etc/systemd/network/20-dhcp.network << EOF
 [Match]
 Name=en*
@@ -29,7 +40,6 @@ DHCP=yes
 ClientIdentifier=mac
 EOF
 
-sudo mv /etc/netplan/*.yaml /etc/netplan/backup/
 sudo systemctl mask systemd-networkd-wait-online.service
 systemctl enable systemd-networkd.service && systemctl enable systemd-resolved.service
 systemctl restart systemd-networkd.service && systemctl restart systemd-resolved.service
